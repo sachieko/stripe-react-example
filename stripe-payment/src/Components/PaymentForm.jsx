9d8bd86310565ee9b7b8917c98a0bf64be074e5b
sachieko <arashikabane@gmail.com>
2022-10-31 18:30:19 -0600

    Install axios, finish stripe component

---
+++ stripe-payment/src/Components/PaymentForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CardNumberElement, CardCvcElement, CardExpiryElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const PaymentForm = function() {
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+export default PaymentForm;
--- stripe-payment/src/Components/Stripe.jsx
+++ stripe-payment/src/Components/Stripe.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import PaymentForm from './PaymentForm';
+
+const stripePromise = loadStripe(`pk_test_51LktkjCljtP53Tab3fCX6fbLY8Rx81LjlBAdOKWpJQKsXwBLR1lulzEMNM2WYa3dSABXbbGQVt4S0yp3SSLQPSoI00cdhPGO1t`);
 
 const Stripe = function() {
   return (
-    <div>
-    </div>
+    <Elements stripe={stripePromise}>
+      <PaymentForm />
+    </Elements>
   );
 };
 
